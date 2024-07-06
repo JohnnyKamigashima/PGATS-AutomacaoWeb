f5073adef4492895b81fd9b98a625e21bebdf47e
Johnny Kamigashima <johnny@jhk.app>
2024-07-06 11:50:32 -0300

    Configuracao inicial TS

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   projectId: 'ekfjcc',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    }
+    // implement node event listeners here
   },
 });
+
