b7282e2abd79ef69ff4e6538726b670e571caab6
Johnny Kamigashima <johnny@jhk.app>
2024-07-20 22:21:32 -0300

    ajustado dependencias

---
+++ cypress.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "cypress";
+import cypressFailFast from 'cypress-fail-fast/plugin.js';
+import cypressMochawesomeReporter from 'cypress-mochawesome-reporter/plugin.js';
+
+export default defineConfig({
+  projectId: 'ekfjcc',
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    baseUrl: 'https://automationexercise.com',
+    env: {
+      logLevel: "VERBOSE",
+      hideXhr: true
+    },
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    defaultCommandTimeout: 10000,
+    numTestsKeptInMemory: 2,
+    specPattern: ["cypress/e2e/**/*.cy.js", "cypress/e2e/**/*.cy.ts"],
+    setupNodeEvents(on, config) {
+      cypressFailFast(on, config);
+      cypressMochawesomeReporter(on);
+      return config;
+    },
+  }
+});
+
